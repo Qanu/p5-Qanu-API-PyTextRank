cbe4a1800f27b909d0259b321919d976726246c7
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-05-22 03:58:34 -0400

    Initial commit

---
+++ lib/Renard/Incunabula/NLP/PyTextRank.pm
@@ -0,0 +1,50 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Incunabula::NLP::PyTextRank;
+# ABSTRACT: Provides access to PyTextRank
+
+use Renard::Incunabula::Common::Types qw(Str);
+use Moo;
+
+use JSON::MaybeXS qw(decode_json);
+use Path::Tiny;
+
+use Inline Python => <<'END';
+import pytextrank
+
+def _get_text_rank(doc_text, path_stage1, path_stage2):
+  doc_text = doc_text.decode('UTF-8')
+  path_stage1 = path_stage1.decode('UTF-8')
+  path_stage2 = path_stage2.decode('UTF-8')
+  doc_data = [ { "id": "777", "text": doc_text} ]
+  with open(path_stage1, 'w') as f:
+    for graf in pytextrank.parse_doc(doc_data):
+        f.write("%s\n" % pytextrank.pretty_print(graf._asdict()))
+  graph, ranks = pytextrank.text_rank(path_stage1)
+  with open(path_stage2, 'w') as f:
+    for rl in pytextrank.normalize_key_phrases(path_stage1, ranks):
+      f.write("%s\n" % pytextrank.pretty_print(rl._asdict()))
+END
+
+method get_text_rank( (Str) $document ) {
+	my $intermediate = Path::Tiny->tempfile;
+	my $output = Path::Tiny->tempfile;
+
+	use Try::Tiny;
+	try {
+		_get_text_rank( "$document", "$intermediate" , "$output");
+	} catch {
+		warn "$_";
+	};
+
+	my @data = map { decode_json($_) } $output->lines_utf8;
+
+	\@data;
+}
+
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-NLP-PyTextRank/>
+
+=cut
---
+++ t/Renard/Incunabula/NLP/PyTextRank.t
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use Test::Most tests => 1;
+
+use Modern::Perl;
+use Renard::Incunabula::NLP::PyTextRank;
+
+subtest "Extract phrases from example text" => sub {
+	my $text = <<EOF;
+Compatibility of systems of linear constraints over the set of natural numbers.
+Criteria of compatibility of a system of linear Diophantine equations, strict
+inequations, and nonstrict inequations are considered. Upper bounds for
+components of a minimal set of solutions and algorithms of construction of
+minimal generating sets of solutions for all types of systems are given.
+These criteria and the corresponding algorithms for constructing a minimal
+supporting set of solutions can be used in solving all the considered types
+systems and systems of mixed types.
+EOF
+
+	my @expected_phrases = (
+		'linear diophantine equations',
+		'linear constraints',
+		'natural numbers',
+	);
+
+	my $tr = Renard::Incunabula::NLP::PyTextRank->new;
+	my $data = $tr->get_text_rank( "$text" );
+
+	cmp_deeply $data, superbagof(
+		map {
+			superhashof( { text => $_ } ),
+		} @expected_phrases
+	);
+};
+
+done_testing;
